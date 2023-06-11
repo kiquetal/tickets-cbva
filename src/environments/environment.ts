872520a24610fb32065a64ef5282924d2b9c13cb
Enrique M. Talavera <kiquetal@gmail.com>
2023-06-11 13:04:36 -0400

    ensure

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: false,
-  supabaseUrl: "https://qufdvxqnxyluphajkayn.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZmR2eHFueHlsdXBoYWprYXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU4NDkxMDMsImV4cCI6MjAwMTQyNTEwM30.Y58I19GeoIuSLeF-3IW2kWvGydJiAEjhcXeLVTtJx3Y"
-}
